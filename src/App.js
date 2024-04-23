1e4e863518bf02f79a182e6b35ae79507ecbb0e5
vishaljadhav7 <vishalj.7661@gmail.com>
2024-04-23 23:45:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-2xl">Hey, there</h1>
+    </div>
+  );
+}
+
+export default App;
